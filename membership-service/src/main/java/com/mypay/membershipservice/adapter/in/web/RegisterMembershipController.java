73a97ff645df95fff0df648fa24682e914771e9d
easydong02 <easydong02@gmail.com>
2025-07-03 21:49:29 +0900

    feat: adapter 추가

--- membership-service/src/main/java/com/mypay/membershipservice/adapter/in/web/RegisterMembershipController.java
+++ membership-service/src/main/java/com/mypay/membershipservice/adapter/in/web/RegisterMembershipController.java
@@ -1,16 +1,24 @@
 package com.mypay.membershipservice.adapter.in.web;
 
 
+import common.WebAdapter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@WebAdapter
 @RestController
 @RequiredArgsConstructor
 public class RegisterMembershipController {
 
-    @GetMapping("/test")
-    void test() {
-        System.out.println("Test endpoint hit");
+    @GetMapping("/membership/register")
+    void registerMembership(@RequestBody RegisterMembershipRequest request) {
+        // request ~
+
+        // request -> command
+
+        // UseCase ~
+
     }
 }
